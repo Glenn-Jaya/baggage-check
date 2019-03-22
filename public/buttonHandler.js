2e34a7fdd2072d731e7b27e10f6e627f5fc26bd0
Glenn-Jaya <jayasugp@myumanitoba.ca>
2019-03-21 21:49:54 -0500

    Refactored code, still need to refactor functions

---
+++ public/buttonHandler.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+function selectButton()
+{
+    if (this.classList.contains("not-selected-btn"))
+    {
+      this.classList.remove("not-selected-btn");
+      this.classList.add("selected-btn");
+      this.value = "selected";
+    }
+    else {
+      this.classList.remove("selected-btn");
+      this.classList.add("not-selected-btn");
+      this.value = "not_selected";
+    }
+}
+
+var buttons = document.getElementsByClassName("not-selected-btn");
+for(i=0; i < buttons.length; i++) {
+  buttons.item(i).addEventListener("click", selectButton);
+}
+
+});
